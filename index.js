4874be9d86b03a8323b5dd66d8ee71a973bdb3ef
milan <58362563+milan1310@users.noreply.github.com>
2020-11-03 11:00:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -39,8 +39,15 @@ app.get("/about",function(req,res){
     res.render('about');
 })
 
- 
 
-app.listen(3000, function(){
-  console.log("Server started on port 3000.");
+var port = process.env.PORT;
+if ( port =="") {
+  port=3000;
+}
+
+
+app.listen(port, function() {
+  console.log("Server started  success fully");
 });
+
+
